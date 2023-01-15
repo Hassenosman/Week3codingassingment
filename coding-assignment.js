7a8c5c7c44ba0564f878d60e55b8995e6c09dff0
Hassenosman <hassanosman200@gmail.com>
2023-01-14 22:16:03 -0600

    Added files

--- coding-assignment.js
+++ coding-assignment.js
@@ -37,14 +37,14 @@ let names= ["sam", "Tommy", "Tom", "Sally", "Buck", "Bob"];
 
 
 /* Use a loop to iterate through the array and calculate the average number of letters per name.  */
-let namestotal = 0;
-let namesavg = 0;
+let namesTotal = 0;
+let namesAvg = 0;
 for(let i = 0; i < names.length; i++) {
-    namesTotal = namestotal + names[i].length;
+    namesTotal = namesTotal + names[i].length;
 }
-namesavg = namestotal/ names.length;
+namesAvg = namesTotal/ names.length;
 
-console.log(namesavg);
+console.log(namesAvg);
 
 // Use a loop to iterate through the array again and concatenate all the names together, separated by spaces.
 let result = "";
@@ -53,3 +53,54 @@ for(let i = 0; i < names.length; i++) {
 }
 
 console.log(result);
+
+// How do you access the last element of any array?
+// arr[arr.length - 1] -> counting starts at 0
+
+// How do you access the first element of any array?
+//arr[0];
+//arr[can do math here]
+
+
+/* Create a new array called nameLengths. Write a loop to iterate over the previously created names array 
+and add the length of each name to the nameLengths array. */
+let newNames = ["Carol", "Roger", "Rob", "Colin"];
+let nameLength = [];
+
+for(let i = 0; i < newNames.length; i++) {
+    nameLength.push(newNames[i].length);
+}
+console.log(nameLength); 
+
+
+/* Write a loop to iterate over the nameLengths array and calculate the sum of all the elements in the array */
+let sum = 0;
+for(let x = nameLength.length; x >= 0; x--){
+    sum += nameLength[x];
+}
+ console.log(sum);
+
+/* Write a function that takes two parameters, word and n, as 
+arguments and returns the word concatenated to itself n number of times. (i.e. if I pass in ‘Hello’ and 3, 
+I would expect the function to return ‘HelloHelloHello’). */
+
+function wordRepeat(word, n){
+    let count = 1;
+    let result = "";
+    while(count <= n){
+        result += word;
+        count++;
+    }
+    return result;
+}
+console.log(wordRepeat('hello', 4));
+
+/* Write a function that takes two parameters, firstName and lastName, and returns a full name. */
+
+function fullName(firstName, lastName){
+    return firstName + " " + lastName;
+}
+
+
+
+
